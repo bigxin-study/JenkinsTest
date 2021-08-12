749013aedc026e11b3c88d4a708b1d86305beb5a
DESKTOP-BHU88JC\zzx <1412755406@qq.com>
2021-08-12 23:45:23 +0800

    new project

---
+++ src/main/java/com/example/jenkinstest/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.jenkinstest.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zzx
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/8/12 23:31
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        System.out.println("*********Bingo**********");
+        return "hello world";
+    }
+}
